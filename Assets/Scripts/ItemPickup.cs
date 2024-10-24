069b2735da5f3a43cc6c5a799db632692fecdd77
Alfie213 <arturbogoratov@gmail.com>
2024-10-24 06:11:16 +0400

    Developing

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ItemPickup : MonoBehaviour
 {
+    private PlayerInputActions _playerControls;
+    
     private Camera _playerCamera;
+    private InputAction _pickupOrPlaceItem;
 
     private GameObject _heldItem;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerInputActions();
+    }
 
     private void Start()
     {
         _playerCamera = Camera.main;
     }
 
-    public void PickupOrPlaceItem()
+    private void OnEnable()
+    {
+        _pickupOrPlaceItem = _playerControls.Player.PickupOrPlaceItem;
+        _pickupOrPlaceItem.Enable();
+        _pickupOrPlaceItem.performed += PickupOrPlaceItem;
+    }
+
+    private void OnDisable()
+    {
+        _pickupOrPlaceItem.Disable();
+        _pickupOrPlaceItem.performed -= PickupOrPlaceItem;
+    }
+
+    public void PickupOrPlaceItem(InputAction.CallbackContext context)
     {
         if (_heldItem == null)
             TryPickupItem();
@@ -30,7 +52,7 @@ public class ItemPickup : MonoBehaviour
                 _heldItem = hit.transform.gameObject;
                 _heldItem.GetComponent<Rigidbody>().isKinematic = true;
                 _heldItem.transform.SetParent(_playerCamera.transform);
-                _heldItem.transform.localPosition = new Vector3(0, 0, 2);
+                _heldItem.transform.localPosition = new Vector3(0, 0, 1);
             }
         }
     }
@@ -43,7 +65,8 @@ public class ItemPickup : MonoBehaviour
         {
             if (hit.transform.CompareTag("PickupTruck"))
             {
-                _heldItem.transform.SetParent(null);
+                _heldItem.transform.SetParent(hit.transform);
+                _heldItem.transform.localPosition = new Vector3(0, 1, 0);
                 _heldItem.GetComponent<Rigidbody>().isKinematic = false;
                 _heldItem = null;
             }
