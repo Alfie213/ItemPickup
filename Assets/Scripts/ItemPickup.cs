f255cfa4665c036c348ea0c61eb0c0ea813fdf60
Alfie213 <arturbogoratov@gmail.com>
2024-10-24 05:10:23 +0400

    Input

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera _playerCamera;
+
+    private GameObject _heldItem;
+
+    private void Start()
     {
-        
+        _playerCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PickupOrPlaceItem()
     {
-        
+        if (_heldItem == null)
+            TryPickupItem();
+        else
+            PlaceItemInPickup();
+    }
+
+    private void TryPickupItem()
+    {
+        Ray ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.transform.CompareTag("PickupItem"))
+            {
+                _heldItem = hit.transform.gameObject;
+                _heldItem.GetComponent<Rigidbody>().isKinematic = true;
+                _heldItem.transform.SetParent(_playerCamera.transform);
+                _heldItem.transform.localPosition = new Vector3(0, 0, 2);
+            }
+        }
+    }
+
+    private void PlaceItemInPickup()
+    {
+        Ray ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.transform.CompareTag("PickupTruck"))
+            {
+                _heldItem.transform.SetParent(null);
+                _heldItem.GetComponent<Rigidbody>().isKinematic = false;
+                _heldItem = null;
+            }
+        }
     }
 }
